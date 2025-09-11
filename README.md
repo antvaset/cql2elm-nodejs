e9b50d137c43b2c193c8728ed37658b78fc50aea
Anton Vasetenkov <antvaset@gmail.com>
2025-09-11 23:38:19 +1200

    everything

---
+++ README.md
@@ -0,0 +1,7 @@
+# cql2elm-nodejs
+
+Run
+
+    node index.mjs
+
+to see example ELM generated from CQL.
---
+++ index.mjs
@@ -0,0 +1,42 @@
+import "./require-polyfill.mjs";
+import {
+  DefaultModelInfoProvider,
+  DefaultLibrarySourceProvider,
+  stringAsPath,
+  ModelManager,
+  LibraryManager,
+  CqlTranslator,
+} from "cql2elm";
+
+const modelManager = new ModelManager();
+modelManager.modelInfoLoader.registerModelInfoProvider(
+  new DefaultModelInfoProvider(stringAsPath("./models")),
+);
+const libraryManager = new LibraryManager(modelManager);
+libraryManager.librarySourceLoader.registerProvider(
+  new DefaultLibrarySourceProvider(stringAsPath("./libraries")),
+);
+
+const cql = `
+library Test version '0.1.0'
+
+using FHIR version '4.0.1'
+
+include FHIRHelpers version '4.0.1'
+
+valueset "Encounter Inpatient": 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.666.5.307'
+
+parameter "Measurement Period" Interval<DateTime>
+
+context Patient
+
+define "Inpatient Encounter":
+  [Encounter: "Encounter Inpatient"] EncounterInpatient
+    where EncounterInpatient.status = 'finished'
+      and EncounterInpatient.period ends during day of "Measurement Period"
+
+`;
+
+const translator = CqlTranslator.fromText(cql, libraryManager);
+
+console.log(translator.toXml());
